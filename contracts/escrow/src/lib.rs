370086cd2f72992604a2c6fb5f6ff6d7ff29a3e1
devfoma <faithamarachi860@gmail.com>
2026-04-26 16:44:39 -0700

    style: fix import formatting in lib.rs

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contracterror, contractimpl, contracttype, Address, Bytes, BytesN, Env,
-    Symbol, Vec,
+    contract, contracterror, contractimpl, contracttype, Address, Bytes, BytesN, Env, Symbol, Vec,
 };
 
 mod ttl;
